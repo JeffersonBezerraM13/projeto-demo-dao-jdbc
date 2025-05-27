aa767b3caf0a3c2e8e1806a50e7fcde75744f3b3
Jefferson Bezerra <jeffebezerram13@gmail.com>
2025-05-27 15:35:40 -0300

    Commit 27/05 - 15:35 findByIdSellerDaoJDBC

---
+++ src/main/java/aplication/Program.java
@@ -0,0 +1,17 @@
+package aplication;
+
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
+import model.dao.impl.SellerDaoJDBC;
+import model.entities.Department;
+import model.entities.Seller;
+
+import java.util.Date;
+
+public class Program {
+    public static void main(String [] args){
+        SellerDao sellerDao = DaoFactory.createSellerDao();
+        Seller seller = sellerDao.findSellerById(3);
+        System.out.println(seller);
+    }
+}
